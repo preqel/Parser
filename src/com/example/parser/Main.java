5aa80d7b22949159ed9010412d06119f4d40a55c
preqel <358795715@qq.com>
2017-08-02 19:00:24 +0800

    second commit

--- src/com/example/parser/Main.java
+++ src/com/example/parser/Main.java
@@ -1,6 +1,8 @@
 package com.example.parser;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,35 +11,26 @@ import java.util.Properties;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		
-		
-		
-		String filePath ="";
-	 
-	               
+	public static void main(String[] args) throws IOException {
+		File file = new File("e:\\a.txt");
+		 InputStream in = new FileInputStream(file);
+		 ByteArrayOutputStream bos = new ByteArrayOutputStream();  
+		 byte[] buffer=new byte[1024];  
+		 int length=-1;  
+		 while( (length = in.read(buffer)) != -1)  {  
+				               bos.write(buffer,0,length);  
+				               // .write方法 SDK 的解释是 Writes count bytes from the byte array buffer starting at offset index to this stream.  
+			               //  当流关闭以后内容依然存在  
+			             }  
+				               bos.close();  
+				  
+				               in.close();  
+				              String result =  bos.toString();
+				              System.out.println(result); 
 
-		String xml = "<xxml></xxml>";
-		
-		 
-		while(true){
-			char[] chars =   xml.toCharArray();
-		
-		}
-		
-//		System.out.println("hello world");
 	}
 	
-	public static String getValueFromFile(String filePath, String key) throws IOException {
-		Properties pps = new Properties();
-		InputStream in = new BufferedInputStream(new FileInputStream(filePath));
-		pps.load(in);
-		String value = pps.getProperty(key);
-		System.out.println(key + " = " + value);
-		
-		
-	}
+ 
 	
 
 }
